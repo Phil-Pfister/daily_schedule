f2ada11fc5d9e8f4c8ce82b3c2d321a9053df006
Phil-Pfister <salshouse@gmail.com>
2023-03-24 18:57:11 -0500

    handled local storage

--- assets/index.js
+++ assets/index.js
@@ -1,34 +1,16 @@
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   var today = dayjs();
+
+  //creates Date header that updates to the current date
 $('#currentDay').text(today.format('dddd, MMM D, YYYY'));
 
+if (dayjs("mm") === 00 && dayjs("ss") === 00) {
+  window.location.reload();
+}
+
 var rootDiv = $('#root-div');
-var outerDiv = $('<div>');
-var hoursEl = $('<div>');
-var textArea = $('<textarea>');
-var addButton = $('<button>');
-var btnIcon = $('<i>');
-var outerDivId;
 
-
-// var hours = [
-//   '9AM',
-//   '10AM',
-//   '11AM',
-//   '12PM',
-//   '1PM',
-//   '2PM',
-//   '3PM',
-//   '4PM',
-//   '5PM',
-// ];
-
+  // Hours variable using dayjs to create elements
 var hours = [
   dayjs().hour(7),
   dayjs().hour(8),
@@ -45,6 +27,7 @@ var hours = [
   dayjs().hour(19),
   dayjs().hour(20),
 ]
+// creates the individual elements for the HTML of the body and appends to the root
 for (var i = 0; i < hours.length; i++) {
 var outerDiv = $('<div>');
 var hoursEl = $('<div>');
@@ -52,28 +35,33 @@ var textArea = $('<textarea>');
 var addButton = $('<button>');
 var btnIcon = $('<i>');
   
-  outerDiv.addClass('row time-block');
-  rootDiv.append(outerDiv);
-  outerDiv.attr("id", 'hour-' + hours[i].format('ha'));
+  // outer div that contains day part elements
+outerDiv.addClass('row time-block');
+rootDiv.append(outerDiv);
+outerDiv.attr("id", 'hour-' + hours[i].format('ha'));
   
+  // element for the hours on the left side of each daypart
+hoursEl.text(hours[i].format('h a'));
+hoursEl.addClass('col-2 col-md-1 hour text-center py-3');
+outerDiv.append(hoursEl);
 
-  
-  hoursEl.text(hours[i].format('h a'));
-  hoursEl.addClass('col-2 col-md-1 hour text-center py-3');
-  outerDiv.append(hoursEl);
-
-
+  // textarea element for updating memos
 textArea.addClass('col-8 col-md-10 description');
 textArea.attr('rows', '3');
+textArea.attr('id', 'text' + hours[i].format('ha'));
 outerDiv.append(textArea);
 
-
+// button element for saving memos
 addButton.addClass('btn saveBtn col-2 col-md-1');
 outerDiv.append(addButton);
+addButton.attr('id', 'btn' + hours[i].format('ha'));
 
-
+// icon of save disk on the buttons
 btnIcon.addClass('fas fa-save');
 addButton.append(btnIcon);
+
+
+// sets class attribute for time period: before, during or after current time
 var now = dayjs().hour();
 if (now < hours[i].hour()) {
   outerDiv.addClass('future');
@@ -86,31 +74,65 @@ else {
   outerDiv.addClass('past');
 
 };
-console.log(outerDiv);
+
 
 }
 
 
+// set to local storage Credit to tutor David Elutilo for help with this
+$('.saveBtn').click(function() {
+  var thisButton = $(this);
+  var textArea = thisButton.siblings('.description');
+  var key = textArea.attr('id');
+  localStorage.setItem(key, textArea.val());
+
+  
+  
+});
+
+    // retrieve from local storage
+var saved7 = localStorage.getItem('text7am');
+text7am.innerHTML = saved7;
+
+var saved8 = localStorage.getItem('text8am');
+text8am.innerHTML = saved8;
+
+var saved9 = localStorage.getItem('text9am');
+text9am.innerHTML = saved9;
+
+var saved10 = localStorage.getItem('text10am');
+text10am.innerHTML = saved10;
+
+var saved11 = localStorage.getItem('text11am');
+text11am.innerHTML = saved11;
+
+var saved12 = localStorage.getItem('text12pm');
+text12pm.innerHTML = saved12;
+
+var saved1 = localStorage.getItem('text1pm');
+text1pm.innerHTML = saved1;
+
+var saved2 = localStorage.getItem('text2pm');
+text2pm.innerHTML = saved2;
+
+var saved3 = localStorage.getItem('text3pm');
+text3pm.innerHTML = saved3;
+
+var saved4 = localStorage.getItem('text4pm');
+text4pm.innerHTML = saved4;
+
+var saved5 = localStorage.getItem('text5pm');
+text5pm.innerHTML = saved5;
+
+var saved6 = localStorage.getItem('text6pm');
+text6pm.innerHTML = saved6;
+
+var saved7p = localStorage.getItem('text7pm');
+text7pm.innerHTML = saved7p;
+
+var saved8p = localStorage.getItem('text8pm');
+text8pm.innerHTML = saved8p;
 
 
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
   });
   
